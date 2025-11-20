0e61c9a4bf1ec5a77bf33f0273700385f4966b30
AlexSemionov <alexnet545@gmail.com>
2025-11-20 03:20:23 +0300

    pagination complete

--- store/simpleapp/templatetags/custom_tags.py
+++ store/simpleapp/templatetags/custom_tags.py
@@ -2,8 +2,26 @@ from datetime import datetime
 from django import template
 
 register = template.Library()
+MONTHS_RU = {
+    1: "января",
+    2: "февраля",
+    3: "марта",
+    4: "апреля",
+    5: "мая",
+    6: "июня",
+    7: "июля",
+    8: "августа",
+    9: "сентября",
+    10: "октября",
+    11: "ноября",
+    12: "декабря",
+}
 
 
 @register.simple_tag()
-def current_time(format_string='%b %d %Y'):
-    return datetime.utcnow().strftime(format_string)
+def current_time():
+    now = datetime.now()
+    day = now.day
+    month = MONTHS_RU[now.month]  # ← родительный падеж
+    year = now.year
+    return f"{day} {month} {year}"
